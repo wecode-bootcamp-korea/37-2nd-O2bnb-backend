d3454ec21041b2ba376484286efccbabf7a74712
dyjeong576 <dyjeong576@gmail.com>
2022-10-05 12:44:25 +0900

    전체제품 가져오기 구현

--- src/models/productDao.js
+++ src/models/productDao.js
@@ -1,1 +1,41 @@
 const dataSource = require('./dataSource')
+
+const getProducts = async (userId, category) => {
+    let tmp = "";
+
+    if (category != "all") tmp = `HAVING p.address like '%${category}%'`
+
+    const result = await dataSource.query(`
+        SELECT 
+            p.id, 
+            p.name,
+            p.price, 
+            p.address, 
+            CASE WHEN l.user_id = ${userId}
+                THEN 
+                    1 
+                ELSE 
+                    0 END AS likeCheck, 
+            JSON_ARRAYAGG(i.image_url) AS image_url 
+        FROM 
+            products p 
+        LEFT JOIN 
+            likes l 
+        ON 
+            p.id = l.product_id AND
+            l.user_id = ${userId} 
+        JOIN 
+            product_images i
+        ON
+            i.product_id = p.id 
+        GROUP BY 
+            p.id ` + tmp
+    )
+
+    return result
+  
+  };
+
+  module.exports = { 
+    getProducts
+}
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const  { validToken } = require('../utils/auth.js');
+
+const productRouter = require('./productRouter');
+
+router.use('/product', validToken, productRouter);
 
 module.exports = router;

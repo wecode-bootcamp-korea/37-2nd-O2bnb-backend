26731dc6a1f32ea572f4a2068831f5ed7695760f
dyjeong576 <dyjeong576@gmail.com>
2022-10-08 21:20:57 +0900

    검색기능 구현

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const router = express.Router();
+const  { validToken } = require('../utils/auth.js');
+
+const productRouter = require('./productRouter');
+
+router.use('/product', validToken, productRouter);
 
 const userRouter = require('./userRouter');
 

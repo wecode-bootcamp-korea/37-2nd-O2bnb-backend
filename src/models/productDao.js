9d76defae3fbdcc9fc7658d745949069fe10ddde
dyjeong576 <dyjeong576@gmail.com>
2022-10-09 17:28:05 +0900

    wip

--- src/models/productDao.js
+++ src/models/productDao.js
@@ -41,7 +41,6 @@ const getProducts = async (userId, category) => {
           }
     })
     
-
     return result
 
   };
--- src/services/productService.js
+++ src/services/productService.js
@@ -2,7 +2,10 @@ const { productDao } = require('../models');
 
 const getProducts = async (userId, category) => {
 
-    return await productDao.getProducts(userId, category);
+    const productInfo = await productDao.getProducts(userId, category);
+    productInfo.review = await productDao.getReviewCount(category);
+
+    return productInfo
 }
 
 module.exports = { 

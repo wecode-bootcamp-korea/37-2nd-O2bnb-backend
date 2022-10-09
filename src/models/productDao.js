ba8d3c5703d1876db3159b29b3f9938a2e2b3fe4
dyjeong576 <dyjeong576@gmail.com>
2022-10-10 01:24:38 +0900

    리뷰 별점 추가

--- src/models/productDao.js
+++ src/models/productDao.js
@@ -8,28 +8,17 @@ const getProducts = async (userId, category) => {
     const result = await dataSource.query(`
         SELECT 
             p.id, 
-            p.name,
-            p.price, 
-            p.address, 
-            CASE WHEN l.user_id = ${userId}
-                THEN 
-                    1 
-                ELSE 
-                    0 END AS likeCheck, 
-            JSON_ARRAYAGG(i.image_url) AS image_url 
-        FROM 
-            products p 
-        LEFT JOIN 
-            likes l 
-        ON 
-            p.id = l.product_id AND
-            l.user_id = ${userId} 
-        JOIN 
-            product_images i
-        ON
-            i.product_id = p.id 
-        GROUP BY 
-            p.id ` + tmp
+            p.name, 
+            p.price,
+            p.address,
+            CASE WHEN l.user_id = ${userId} THEN 1 ELSE 0 END AS checkLike,
+            AVG(r.clean_star+r.address_star+r.price_star)/3 AS reviewStar,
+            JSON_ARRAYAGG(i.image_url) AS image_url
+        FROM products p 
+        LEFT JOIN likes l ON l.product_id = p.id AND l.user_id = ${userId}
+        LEFT JOIN reviews r ON r.product_id = p.id 
+        JOIN product_images i ON i.product_id = p.id 
+        GROUP BY p.id ` + tmp
     )
     result.map(el => {
         if(typeof el.image_url == "string"){
--- src/services/productService.js
+++ src/services/productService.js
@@ -3,7 +3,6 @@ const { productDao } = require('../models');
 const getProducts = async (userId, category) => {
 
     const productInfo = await productDao.getProducts(userId, category);
-    productInfo.review = await productDao.getReviewCount(category);
 
     return productInfo
 }

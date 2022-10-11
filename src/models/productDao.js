de11131651b670929e8915ec886ed77cb999bd40
dyjeong576 <dyjeong576@gmail.com>
2022-10-11 19:04:56 +0900

    map 추가

--- src/models/productDao.js
+++ src/models/productDao.js
@@ -1,1 +1,26 @@
 const dataSource = require('./dataSource')
+
+const getMap = async (keyword) => {
+    let tmp = "";
+
+    if (keyword != "all") tmp = `WHERE address like '%${keyword}%'`
+
+    const result = await dataSource.query(`
+        SELECT 
+            id, 
+            name, 
+            address, 
+            latitude, 
+            longitude 
+        FROM 
+            products 
+        ` + tmp
+    )
+
+    return result
+
+  };
+
+  module.exports = { 
+    getMap
+}

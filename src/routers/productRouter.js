aaa058450fb069600afeae337f615bbbe2367f36
dyjeong576 <dyjeong576@gmail.com>
2022-10-11 14:20:39 +0900

    wip

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { productController }  = require('../controllers');
+const { validToken } = require('../utils/auth');
 const router = express.Router();
 
-router.get('/:category', productController.getProducts);
+router.get('/:category', validToken,productController.getProducts);
 
 module.exports = router;

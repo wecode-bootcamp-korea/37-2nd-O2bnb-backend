186b1979e129b8125921b5dc591f9653be339afe
dyjeong576 <dyjeong576@gmail.com>
2022-10-13 10:59:28 +0900

    book validToken 추가

--- src/routers/bookRouter.js
+++ src/routers/bookRouter.js
@@ -5,11 +5,11 @@ const { validToken } = require('../utils/auth');
 
 const router = express.Router();
 
-router.post('/:productId', bookController.makeBooking);
-router.get('/all', bookController.getAllBookings);
-router.get('/order', bookController.checkBookingInfo);
-router.patch('/confirm/order', bookController.confirmBooking);
-router.get('/payment', bookController.completeBooking);
-router.patch('/cancel/:productId', bookController.cancelBooking);
+router.post('/:productId', validToken, bookController.makeBooking);
+router.get('/all', validToken, bookController.getAllBookings);
+router.get('/order', validToken, bookController.checkBookingInfo);
+router.patch('/confirm/order', validToken, bookController.confirmBooking);
+router.get('/payment', validToken ,bookController.completeBooking);
+router.patch('/cancel/:productId', validToken, bookController.cancelBooking);
 
 module.exports = router;
